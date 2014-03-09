a3b1f5593a381e78732619033ab624db6d7bfc7c
Nick Clark <nickclark1000@gmail.com>
2014-03-08 19:28:47 -0500

    added geometry libraries

--- Prepare/PlanGenerator.php
+++ Prepare/PlanGenerator.php
@@ -1,12 +1,14 @@
 <?php
 
 include '../Common/admininfo.php';
+include_once('../Common/geoPHP/geoPHP.inc');
+include '../Common/spherical-geometry-php/spherical-geometry.class.php';
 
 $CourseId = 1; //$_POST['courseId'];
 $UserId = $_POST['userId'];
 
 //Get hole lengths (e.g., 400 yds) into an array
-$HoleDistance = mysqli_query($conn, "select Yardage from Holes where CourseId = '$CourseId'")or die(mysqli_error());
+$HoleDistance = mysqli_query($conn, "select Yardage from Holes where CourseId = '$CourseId'") or die(mysqli_error());
 while($row = mysqli_fetch_array($HoleDistance))
 {
 $HoleYardageArray[] = $row['Yardage'];
@@ -21,15 +23,65 @@ $ClubNum = 1;
 $avgDrive = mysqli_query($conn, "SELECT avg(ShotDistance) FROM Shots where ShotNum=1 and ClubNum='$ClubNum'") or die(mysqli_error());
 $resAvgDrive = mysqli_fetch_array($avgDrive)[0];
 
-echo getDistance(45.99981, -82.93847, 45.99999, -82.93999)." yards";
+//echo getDistance(45.99981, -82.93847, 45.99999, -82.93999)." yards";
+
+$HoleNum=1;
+//Pick point on fairway polyline that lies 300yds from tee box or closest to 300 yds.
+$TargetLine = mysqli_query($conn, "SELECT AsText(FeatureValue) from HoleInfo where FeatureType = 'targetline' and CourseId = '$CourseId' and HoleNum = '$HoleNum'") or die(mysqli_error());
+$TargetResult = mysqli_fetch_array($TargetLine)[0];
+$polyline = geoPHP::load($TargetResult,'wkt');
+$polyline_array = $polyline->getComponents();
+
+$TeemarkerQuery = mysqli_query($conn, "SELECT AsText(FeatureValue) from HoleInfo where FeatureType = 'teemarker' and CourseId = '$CourseId' and HoleNum = '$HoleNum'") or die(mysqli_error());
+$TeemarkerResult = mysqli_fetch_array($TeemarkerQuery)[0];
+$teemarker = geoPHP::load($TeemarkerResult, 'wkt');
+
+//initial conditions
+$MinDistance = 9999;
+$valueid = 0;
+foreach ($polyline_array as $key=>$value) {
+	$pt1 = new LatLng($value->y(),$value->x());
+	$pt2 = new LatLng($teemarker->y(), $teemarker->x());
+	$distance = SphericalGeometry::computeDistanceBetween($pt1, $pt2);
+	$dDist = $resAvgDrive - $distance;
+
+	if($dDist < $MinDistance) {
+		$MinDistance = $dDist;
+		$valueid = $key;
+	}
+}
+//echo $MinDistance.",".$valueid.",".$resAvgDrive;
+
+$TeeblockQuery = mysqli_query($conn, "SELECT AsText(FeatureValue) from HoleInfo where FeatureType = 'teeblock' and CourseId = '$CourseId' and HoleNum = '$HoleNum'") or die(mysqli_error());
+$TeeblockResult = mysqli_fetch_array($TeeblockQuery)[0];
+$teeblock = geoPHP::load($TeeblockResult, 'wkt');
+
+if (SphericalGeometry::containsLocation($teemarker, $teeblock)==true){
+  echo "Is in polygon!";
+}
+else echo "Is not in polygon";
+
+//Get heading between tee shot location and pt from 3
+$lon1 = $teemarker->x();
+$lat1 = $teemarker->y();
+$TargetLon = $polyline_array[$valueid]->x();
+$TargetLat = $polyline_array[$valueid]->y();
+
+$TargetPt = new LatLng($TargetLat, $TargetLon);
+$TeemarkerPt = new LatLng($lat1, $lon1);
+
+$heading = SphericalGeometry::computeHeading($TeemarkerPt, $TargetPt);
+echo "....".SphericalGeometry::computeDistanceBetween($TeemarkerPt, $TargetPt);
+
+//Get perpendicular headings
+$headingR90 = $heading + 90;
+$headingL90 = $heading - 90;
+
+//With 1st heading, use 3 to get distance to edge of right fairway
 
 
 
 /*
-Pick point on fairway polyline that lies 300yds from tee box or closest to 300 yds.
-Get heading between tee shot location and pt from 3
-Get perpendicular headings
-With 1st heading, use 3 to get distance to edge of right fairway
 With 2nd heading, use 3 to get distance to edge of left fairway
 Get L or R of target with random number and probability model
 Get avg distance off target when L or R
@@ -46,38 +98,21 @@ Get avg distance off target when L or R
 Get generated shot location #2
 
 
-//Define distance ranges	
-$distRanges = array(0,10,20,30,40,50,60,70,80,90,100);
-$UpDownAttempts = array();
-$UpDownSuccesses = array();
-
-for($i=0; $i < count($distRanges)-1; $i++) {
-	$j = $i + 1;
-	$successes = mysqli_query($conn, "select count(*) from Shots a INNER JOIN Rounds b ON a.RoundId = b.RoundId INNER JOIN Holes c ON b.CourseId = c.CourseId where a.HoleNum = c.HoleNum and a.DistanceToHole <= ".$distRanges[$j]." and a.DistanceToHole > ".$distRanges[$i]." and a.ShotFrom != 'green' and (a.ShotNum = (SELECT MAX(ShotNum) FROM Shots e where e.RoundId = a.RoundId and e.HoleNum = a.HoleNum) OR a.ShotNum + 1 = (SELECT MAX(ShotNum) FROM Shots e where e.RoundId = a.RoundId and e.HoleNum = a.HoleNum))") or die(mysqli_error($conn));
-	$UpDownSuccesses[$distRanges[$i]."-".$distRanges[$j]] = mysqli_fetch_array($successes)[0];
-	$attempts = mysqli_query($conn, "select count(*) from Shots a INNER JOIN Rounds b ON a.RoundId = b.RoundId INNER JOIN Holes c ON b.CourseId = c.CourseId where a.HoleNum = c.HoleNum and a.DistanceToHole <= ".$distRanges[$j]." and a.DistanceToHole > ".$distRanges[$i]." and a.ShotFrom != 'green'") or die(mysqli_error($conn));
-	$UpDownAttempts[$distRanges[$i]."-".$distRanges[$j]] = mysqli_fetch_array($attempts)[0];
-}
-
-$UpDown = array("Attempts"=>$UpDownAttempts,"Successes"=>$UpDownSuccesses);
-echo json_encode($UpDown);
-*/
-
 //Haversine formula. Return distance in yards
-function getDistance($lat1, $lng1, $lat2, $lng2) {
+function getDistance($lat1, $lon1, $lat2, $lon2) {
 	//Earth radius in yards
 	$EarthRadius = 6371 * 1000 * 1.09361;
 	
 	$dLat = deg2rad($lat2 - $lat1);
-	$dLng = deg2rad($lng2 - $lng1);
+	$dLon = deg2rad($lon2 - $lon1);
 	
-	$a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLng/2) * sin($dLng/2);
+	$a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon/2) * sin($dLon/2);
 	$c = 2 * asin(sqrt($a));
 	$d = $EarthRadius * $c;
 	
 	return $d;
 }
-
+*/
 //close connection
 mysqli_close($conn);
 
